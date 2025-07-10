da3defcfb35bc6e893d317ad4c4ce2990565d4ec
spotter002 <spotterwamai@gmail.com>
2025-07-10 09:37:48 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,24 @@
+// Entry File
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
+require('dotenv').config()
+
+// middlewares
+const app = express()
+app.use(express.json())
+app.use(cors())
+
+// static files accessibility
+app.use('/uploads', express.static('uploads'))
+
+// connect to the database
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => console.log('Connected to MongoDB'))
+    .catch(err => console.error('Could not connect to MongoDB:', err))
+
+
+const PORT = 3000
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`)
+})
