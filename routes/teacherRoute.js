55b3485910ea97f7ae7be60b97a73247f528c619
spotter002 <spotterwamai@gmail.com>
2025-08-06 09:12:08 +0300

    Update teacherRoute.js

--- routes/teacherRoute.js
+++ routes/teacherRoute.js
@@ -12,8 +12,8 @@ router.get('/', auth, teacherController.getAllTeachers)
 router.get('/classes',auth,teacherController.getMyClasses)
 router.get('/assignments',auth,teacherController.getMyAssignments)
 
-router.put('/self', auth, authorizeRoles('teacher'), teacherController.updateTeacher)
 router.delete('/:id', auth, authorizeRoles('admin'), teacherController.deleteTeacher)
 router.get('/:id', auth, teacherController.getTeacherById)
 router.put('/:id', auth, authorizeRoles('admin','teacher'), teacherController.updateTeacher)
+router.put('/self', auth, authorizeRoles('teacher'), teacherController.updateTeacher)
 module.exports = router
