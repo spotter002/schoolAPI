dab58e8354903a3b7941141e375caf8258cddc42
spotter002 <spotterwamai@gmail.com>
2025-08-08 09:23:47 +0300

    Update studentRoute.js

--- routes/studentRoute.js
+++ routes/studentRoute.js
@@ -2,14 +2,26 @@ const express = require('express')
 const router = express.Router()
 const studentController = require('../controller/studentController')
 const {auth, authorizeRoles} = require('../middleware/auth')
-
+const multer = require('multer');
+const path = require('path');
 
 
 // classroom routes
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'uploads/students'); // Make sure this folder exists
+  },
+  filename: (req, file, cb) => {
+    cb(null, `${Date.now()}-${file.originalname}`);
+  },
+});
+
+const upload = multer({ storage });
+
 router.post('/addStudent', auth, authorizeRoles('admin'), studentController.uploadStudentPhoto, studentController.addStudent)
 router.get('/', auth,studentController.getAllStudents)
 router.get('/:id', auth,studentController.getStudentById)
 router.put('/:id', auth, authorizeRoles('admin'), studentController.updateStudent)
 router.delete('/:id', auth, authorizeRoles('admin'), studentController.deleteStudent)
-
+router.put('/:id', upload.single('photo'), updateStudent);
 module.exports = router
